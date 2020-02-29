fed88cda5380c18a85a42cd333056c0a0e91dd5b
Evgeny Mamontov <mamontovevgeny@mail.ru>
2020-02-29 23:06:02 +0300

    Added TradingService class

---
+++ src/main/java/ru/emamontov/tradingService/TradingService.java
@@ -0,0 +1,11 @@
+package ru.emamontov.tradingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingService {
+    public static void main(String[] args) {
+        SpringApplication.run(TradingService.class, args);
+    }
+}
