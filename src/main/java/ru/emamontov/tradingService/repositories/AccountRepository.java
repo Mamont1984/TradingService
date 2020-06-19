5b8b5b95f22ae1524aa00f922a6cc10311002e4d
Evgeny Mamontov <mamontovevgeny@mail.ru>
2020-06-19 21:14:58 +0300

    Create AccountRepository

--- src/main/java/ru/emamontov/tradingService/repositories/AccountRepository.java
+++ src/main/java/ru/emamontov/tradingService/repositories/AccountRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository<User, Long> extends CrudRepository<User, Long> {
+public interface AccountRepository<Account, Long> extends CrudRepository<Account, Long> {
 }
