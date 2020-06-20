fc0d17974fa8cae4362dea40e2e407744868c4da
Evgeny Mamontov <mamontovevgeny@mail.ru>
2020-06-20 15:41:36 +0300

    Add lombok to project

--- src/main/java/ru/emamontov/tradingService/entities/Account.java
+++ src/main/java/ru/emamontov/tradingService/entities/Account.java
@@ -1,36 +1,24 @@
 package ru.emamontov.tradingService.entities;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name="accounts")
 public class Account implements Serializable {
     @Id
     @GeneratedValue
+    @EqualsAndHashCode.Include
     private long id;
+
     private long balance;
+
     @ManyToOne
     @JoinColumn(name = "users_id")
     private User user;
-
-    public long getId() {
-        return id;
-    }
-
-    public long getBalance() {
-        return balance;
-    }
-
-    public void setBalance(long balance) {
-        this.balance = balance;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
--- src/main/java/ru/emamontov/tradingService/entities/User.java
+++ src/main/java/ru/emamontov/tradingService/entities/User.java
@@ -1,42 +1,24 @@
 package ru.emamontov.tradingService.entities;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.Set;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name="users")
 public class User implements Serializable {
     @Id
     @GeneratedValue
+    @EqualsAndHashCode.Include
     private long id;
+
     private String name;
+
     @OneToMany
     private Set<Account> accounts;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
